4fa8883712b4e347699fb1982158d9d31479b70f
Stanislav-003 <140276613+Stanislav-003@users.noreply.github.com>
2024-04-19 16:41:15 +0300

    HW1
    HW1

---
+++ HW1/Product.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HW_1._1
+{
+    //[Table("Product")]
+    public class Product
+    {
+        [Key]
+        [Column("ProductId")]
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public double Cost { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? CreatedDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
